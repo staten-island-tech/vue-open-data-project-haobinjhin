8adccf80a33749ac54f27be51ad04a9ce7e95daa
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2025-02-26 12:27:04 -0500

    fdsfdsfsf

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import {ref, onMounted} from 'vue'
+
+const fireincident = ref("")
+async function getincident(){
+
+  try {
+    let response = await fetch('https://data.cityofnewyork.us/resource/ii3r-svjz.json')
+    let data = await response.json();
+    fireincident.value = data;
+    console.log(fireincident.value)
+  } catch (error){
+    console.log("error")
+  }
+
+}
+
+getincident()
+
+
+
+
+
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
