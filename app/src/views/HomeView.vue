e10503285f600d4999e92b4ed9a8eef585e78a58
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2025-03-19 19:12:09 -0400

    sdffdssfd

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -3,64 +3,50 @@ import IncidentsCard from '@/components/IncidentsCard.vue'
 import { ref, onMounted, onBeforeMount } from 'vue'
 import { compileScript } from 'vue/compiler-sfc'
 
-let boroughCount = 0
+
 
 async function getincident() {
   try {
     let res = await fetch('https://data.cityofnewyork.us/resource/ii3r-svjz.json/')
     const data = await res.json()
-    filterincident(data)
     return data
   } catch (error) {
     console.log('error')
   }
 }
 
-function filterincident(incidents) {
-  incidents.forEach((incident) => {
-    if (incident.borough == 'Brooklyn') {
-      console.log(incident)
-    }
-  })
-}
+const allIncidents = ref([])
+
 
 onBeforeMount(() => {})
 
-onMounted(() => {
-  getincident()
-  /* allIncidents.forEach((incident) => {
-     things.forEach((item) => {
-      if (item.borough == 'Brooklyn') {
-        console.log(item)
-      }
-    })
-    console.log(things) 
-    if (incident.borough == 'Brooklyn') {
-      console.log(incident)
-    }
-  }) */
+onMounted(async () => {
+  try {
+    allIncidents.value = await getincident()
+    let boroughCount = 0
+    let percentage = ref([])
+    const boroughs = ['Brooklyn', 'Queens', 'Manhattan', 'Bronx', 'Staten Island']
 
-  const boroughs = ['Brooklyn', 'Queens', 'Manhattan', 'Bronx', 'Staten Island']
-
-  let percentage = []
-
-  /* boroughs.forEach((singleborough) => {
-    allIncidents.filter((innerlist) => {
-      innerlist.forEach((maybeborough) => {
-        if (maybeborough == singleborough) {
-          boroughCount++
-        } else {
-          boroughCount = 1
+    boroughs.forEach((borough) => {
+      allIncidents.value.forEach((incident) => {
+        if(incident.borough == borough){
+          boroughCount ++
         }
       })
+      percentage.value.push(Math.round((boroughCount/allIncidents.value.length)*1000)/10)
+      boroughCount = 0
+
+
+      /* Math.round(num*10)/10 */
     })
+    console.log(allIncidents.value.length)
+    console.log(percentage.value)
 
-    let boroughpercent = (boroughCount / allIncidents.length) * 100
-    percentage.push(boroughpercent)
-    boroughCount = 0
-  }) */
 
-  /* console.log(percentage) */
+  } catch (error){
+    console.log('error')
+  }
+
 })
 </script>
 
